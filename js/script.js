d71305428390b29e75649ebda64b290565477e3b
Zaed Tonmoy <zaedtonmoy@gmail.com>
2021-02-25 21:29:02 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+  // wow
+  wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
+  // wow
+
+  // init Isotope
+  var $grid = $('.grid').isotope({
+    itemSelector: '.color-shape'
+  });
+  // store filter for each group
+  var filters = {};
+  $('.filters').on( 'click', '.button', function( event ) {
+    var $button = $( event.currentTarget );
+    // get group key
+    var $buttonGroup = $button.parents('.button-group');
+    var filterGroup = $buttonGroup.attr('data-filter-group');
+    // set filter for group
+    filters[ filterGroup ] = $button.attr('data-filter');
+    // combine filters
+    var filterValue = concatValues( filters );
+    // set filter for Isotope
+    $grid.isotope({ filter: filterValue });
+  });
+  // change is-checked class on buttons
+  $('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function( event ) {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      var $button = $( event.currentTarget );
+      $button.addClass('is-checked');
+    });
+  });
+  // flatten object by concatting values
+  function concatValues( obj ) {
+    var value = '';
+    for ( var prop in obj ) {
+      value += obj[ prop ];
+    }
+    return value;
+  }
+  // isotope
+
+  // parallax
+  $('.parallax-window').parallax({imageSrc: '/path/to/image.jpg'});
+  // parallax
+
+  // Slick
+  $('.center').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    speed: 600,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+  // Slick
+
+  // owl
+  var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
+  // owl
+})
